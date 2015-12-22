a34e9d1eb31dbbb3b038c68185c19734471fb79d
Arka Roy <arka.nitdgp14@gmail.com>
2015-12-23 04:04:23 +0530

    3rd commit

--- sqlfile.sql
+++ sqlfile.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2015 at 10:22 PM
+-- Generation Time: Dec 22, 2015 at 11:33 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,6 +23,97 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `class`
+--
+
+CREATE TABLE IF NOT EXISTS `class` (
+`id` int(11) NOT NULL,
+  `teacherusername` varchar(255) NOT NULL,
+  `class` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`id`, `teacherusername`, `class`) VALUES
+(1, 'teacher1', 2),
+(3, 'teacher1', 4),
+(4, 'teacher1', 6),
+(5, 'teacher2', 8),
+(6, 'teacher2', 1),
+(7, 'teacher3', 4),
+(8, 'teacher5', 4),
+(9, 'teacher4', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `roll` int(10) NOT NULL,
+  `class` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1 COMMENT='students table';
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `name`, `roll`, `class`) VALUES
+(1, 'Arka Roy', 1, 1),
+(2, 'Debpriya Piri', 8, 8),
+(3, 'Akash Saha', 2, 1),
+(4, 'Suprotim Das', 3, 2),
+(5, 'Adarsh Singh', 4, 4),
+(6, 'Shrey Sharma', 5, 4),
+(7, 'Aakash Basu', 6, 6),
+(8, 'Anmol Verma', 7, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `roll` int(11) NOT NULL,
+  `sub1` int(11) NOT NULL,
+  `sub2` int(11) NOT NULL,
+  `sub3` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `attendance` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='subjects table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE IF NOT EXISTS `teacher` (
+`id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `roll` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`id`, `username`, `name`, `roll`) VALUES
+(1, 'teacher1', 'Mr. X', 1),
+(2, 'teacher2', 'Mr. Y', 2),
+(3, 'teacher3', 'Mr. Z', 4),
+(4, 'teacher4', 'Mr. W', 6);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,18 +122,40 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `type` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `type`) VALUES
-(1, 'arka', 'helloworld', 1);
+(1, 'arka', 'helloworld', 2),
+(2, 'teacher1', 'helloworld', 2),
+(3, 'teacher4', 'helloworld', 2),
+(4, 'teacher2', 'helloworld', 2),
+(5, 'teacher3', 'helloworld', 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -55,10 +168,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
