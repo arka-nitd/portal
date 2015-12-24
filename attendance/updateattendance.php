9583bd9373b83ad9ab30a81a5690e4990dd8e8f8
Arka Roy <arka.nitdgp14@gmail.com>
2015-12-25 04:24:42 +0530

    attendance db added

--- attendance/updateattendance.php
+++ attendance/updateattendance.php
@@ -3,10 +3,17 @@
 $result = mysqli_query($db,"SELECT class_id FROM class WHERE teacher_id='$uid'");
 $usercount = mysqli_num_rows($result);
 if($usercount>0){
-	echo'<ul class="nav nav-tabs nav-justified">';
+	echo'<ul class="nav  nav-justified"> <h4><br>Give Attendance to the following Classes:</h4>';
 	while ($row = $result->fetch_assoc()) {
 		$class=$row['class_id'];
-		echo'<li role="presentation"><a href="class.php?cid='.$class.'">Class '.$class.'</a></li>';
+		echo'<li role="presentation"><a href="editclass.php?cid='.$class.'">Class '.$class.'</a></li>';
+	}
+	echo'</ul>';
+	$result2 = mysqli_query($db,"SELECT class_id FROM class WHERE teacher_id='$uid'");
+	echo'<ul class="nav  nav-justified"> <h4><br>View Attendance of the following Classes:</h4>';
+	while ($row1 = $result2->fetch_assoc()) {
+		$class=$row1['class_id'];
+		echo'<li role="presentation"><a href="viewclass.php?cid='.$class.'">Class '.$class.'</a></li>';
 	}
 	echo'</ul>';
 }
---
+++ editclass.php
@@ -0,0 +1,68 @@
+<?php include ('./inc/header.inc.php');
+
+//attendance submit code
+if (isset($_POST['attendancesubmit'])) {
+	$clsid = $_POST['classid'];
+	$result = mysqli_query($db,"SELECT *  FROM student WHERE class_id='$clsid'");
+	while ($row = $result->fetch_assoc()) {
+		$sid = $row['student_id'];
+		if(isset($_POST[$sid])){
+			$sql=mysqli_query($db,"INSERT INTO `portal2`.`attendance` (`student_id`, `date`, `teacher_id`, `record`) 
+				VALUES ('$sid', CURRENT_TIMESTAMP, '$uid', '1')");
+		}
+		else{
+			$sql=mysqli_query($db,"INSERT INTO `portal2`.`attendance` (`student_id`, `date`, `teacher_id`, `record`) 
+				VALUES ('$sid', CURRENT_TIMESTAMP, '$uid', '0')");
+		}
+	}
+	echo "Updated";
+}
+
+echo "<h2>Update the attendance : Class ";
+if(isset($_GET['cid'])){
+
+	$cid = mysqli_real_escape_string($db,$_GET['cid']);
+
+	$result = mysqli_query($db,"SELECT name, roll, student_id  FROM student WHERE class_id='$cid'");
+	echo $cid."</h2><br>";
+	$usercount = mysqli_num_rows($result);
+	if($usercount>0){
+		echo 
+		'<table class="table table-bordered">
+		<thread><tr>
+		<th>#</th>
+		<th>Roll No</th>
+		<th>Student Name</th>
+		<th>Present/Absent</th>
+		</tr></thread>
+		<tbody>
+		<form role="form" method="POST" action="class.php">';
+		$p=0;
+		while ($row = $result->fetch_assoc()) {
+			$name=$row['name'];
+			$rollno = $row['roll'];
+			$sid = $row['student_id'];
+			echo
+			'
+			<tr>
+				<th scope="row">'.($p+1).'</th>
+				<td>'.$rollno.'</td>
+				<td>'.$name.'</td>
+				<td><input type="checkbox" checked name="'.$sid.'" /></td>
+			</tr>
+			';
+			$p++;
+		}
+				echo '<input type="hidden" name="classid" value="'.$cid.'" />';
+		echo'
+		</tbody>
+		</table>
+		<button type="submit" class="btn btn-default" name="attendancesubmit">Submit</button>
+		</form>';
+	}
+}
+
+
+
+include("./inc/footer.inc.php"); ?>
+
